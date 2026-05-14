92ae023e3a582131d882d28fce82a48ad74129db
XCantis <elliottfabienne2@gmail.com>
2026-05-14 06:15:37 -0400

    Add constants for roles, coat colors, and traits

--- game/constants.py
+++ game/constants.py
@@ -1,1 +1,63 @@
+ROLES = [
+    "King",
+    "Queen",
+    "Guard",
+    "Hunter",
+    "Herbalist",
+    "Sitter",
+    "Mentor",
+    "Juvenile",
+    "Kit"
+]
 
+COAT_COLORS = [
+    "Black",
+    "Grey",
+    "White",
+    "Cream",
+    "Tan",
+    "Brown",
+    "Dark Brown",
+    "Maltese",
+    "Red"
+]
+
+MARKINGS = [
+    "Spots",
+    "Rosettes",
+    "Stripes",
+    "Deer Spots",
+    "Marble",
+    "Tabby",
+    "Calico",
+    "Tabby Calico"
+]
+
+COAT_TYPES = [
+    "Short",
+    "Medium",
+    "Long",
+    "Winter",
+    "Rough",
+    "Curly"
+]
+
+MANES = [
+    "Roached",
+    "Mohawk",
+    "Short",
+    "Tall",
+    "Hyena Spiked"
+]
+
+MUTATIONS = [
+    "Piebald",
+    "Albino",
+    "Melanistic",
+    "Erythristic",
+    "Polycudal",
+    "Blind",
+    "Deaf",
+    "Overgrown Tongue",
+    "No Tail"
+]
